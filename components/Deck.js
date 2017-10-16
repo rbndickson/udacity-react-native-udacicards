d2c1bb100da88522250b6b98a5e5600b66a52137
Robin Dickson <robindickson@gmail.com>
2017-10-16 15:17:14 +0200

    feat: Add deck screen layout

--- components/Deck.js
+++ components/Deck.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import {Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import { white, blue, black } from '../utils/colors';
 
 class Deck extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -10,11 +12,52 @@ class Deck extends Component {
 
   render() {
     return (
-      <View>
-        <Text>...</Text>
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Add New Card</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Take Quiz</Text>
+        </TouchableOpacity>
       </View>
     )
   }
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingTop: 50,
+    backgroundColor: white
+  },
+  textInstructions: {
+    width: 200
+  },
+  textInput: {
+    width: 200,
+    height: 40,
+    marginTop: 20,
+    borderColor: blue,
+    borderWidth: 1,
+    color: black
+  },
+  button: {
+    height: 45,
+    width: 200,
+    backgroundColor: white,
+    padding: 10,
+    marginTop: 20,
+    borderRadius: 7,
+    borderColor: blue,
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: blue,
+    fontSize: 18,
+    textAlign: 'center'
+  }
+})
 
 export default Deck;
