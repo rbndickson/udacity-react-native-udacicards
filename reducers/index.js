4b4b6824800ada8b9cadd72039566bad662cc42c
Robin Dickson <robindickson@gmail.com>
2017-10-16 20:26:05 +0200

    chore: Remove example data

--- reducers/index.js
+++ reducers/index.js
@@ -1,31 +1,6 @@
 import { ADD_DECKS, ADD_DECK } from '../actions';
 
-const initialState = {
-  React: {
-    title: 'React',
-    cards: [
-      {
-        front: 'What is React?',
-        back: 'A JavaScript library for building user interfaces'
-      },
-      {
-        front: 'When was React released?',
-        back: 'March 2013'
-      }
-    ]
-  },
-  Redux: {
-    title: 'Redux',
-    cards: [
-      {
-        front: 'What is redux?',
-        back: 'A predictable state container for JavaScript apps'
-      }
-    ]
-  }
-}
-
-function decks (state = initialState, action) {
+function decks (state = {}, action) {
   switch (action.type) {
     case ADD_DECKS :
       return {
